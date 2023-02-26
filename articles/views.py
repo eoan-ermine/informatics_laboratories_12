e9c1b90fd4d3370cb0e8fd4ddfa08c2b5b15656c
PatriotRossii <patriotrossii2019@mail.ru>
2023-02-26 21:52:52 +0300

    Implement create article endpoint

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from .models import Article
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 
 
@@ -12,4 +12,22 @@ def get_article(request, article_id):
 		post = Article.objects.get(id=article_id)
 		return render(request, "article.html", {"post": post})
 	except Article.DoesNotExist:
+		raise Http404
+
+
+def create_post(request):
+	if not request.user.is_anonymous:
+		if request.method == "POST":
+			form = {
+				"text": request.POST["text"], "title": request.POST["title"]
+			}
+			if form["text"] and form["title"]:
+				article = Article.objects.create(text=form["text"], title=form["title"], author=request.user)
+				return redirect("get_article", article_id=article.id)
+			else:
+				form["errors"] = "Не все поля заполнены"
+				return render(request, "create_post.html", {"form": form})
+		else:
+			return render(request, "create_post.html")
+	else:
 		raise Http404
--- blog/urls.py
+++ blog/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.archive, name="archive"),
     path('article/<int:article_id>', views.get_article, name='get_article'),
+    path('article/add/', views.create_post, name='create_post'),
 ]
